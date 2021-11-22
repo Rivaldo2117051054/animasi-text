7d4ad69dd28d165de23260de095e67a1cfd99b5e
Rivaldo2117051054 <rivaldorr547@gmail.com>
2021-11-23 06:48:31 +0700

    animasi text simple

---
+++ ani text.cpp
@@ -0,0 +1,46 @@
+#include <ncurses/ncurses.h>
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+/* Nama : Rivaldo
+   Kelas: B
+   NPM: 2117051054 */
+   
+int main(){
+	int i=0,z=2,o=14,p=46;
+	initscr();
+while(i<30){
+	mvprintw(9,i++,"=");
+	refresh();
+	Sleep(200);
+	
+	mvprintw(10,z++," LOADING");
+	refresh();
+	Sleep(200);
+	
+	
+	mvprintw(11,i++,"=");
+	refresh();
+	Sleep(200);
+	
+	mvprintw(13,o--,"<");
+	refresh();
+	Sleep(200);
+	
+	mvprintw(13,p--,">");
+	refresh();
+	Sleep(200);
+	
+}
+	mvprintw(13,15," Namaku Rivaldo");
+	refresh();
+	Sleep(200);
+	
+	
+
+
+	getch();
+	endwin();
+
+}
